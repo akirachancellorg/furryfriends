eb9a7838797946f760f8d95e4692ea9a568516dc
Jorenzo Martin Reyes <jjreyes@student.apc.edu.ph>
2024-10-06 22:24:34 +0800

    Removed unnecessary updateDog function in DogService

--- src/main/java/com/entjava/furryfriends/service/DogService.java
+++ src/main/java/com/entjava/furryfriends/service/DogService.java
@@ -17,25 +17,6 @@ public class DogService {
     }
 
     public Dog saveDog(Dog dog) {return dogRepository.save(dog);}
-    
-    public Dog updateDog(Long id, Dog updatedDog) {
-        Optional<Dog> optionalDog = dogRepository.findById(id);
-
-        if (optionalDog.isPresent()) {
-            Dog existingDog = optionalDog.get();
-
-            // Update the existing dog's properties
-            existingDog.setName(updatedDog.getName());
-            existingDog.setBreed(updatedDog.getBreed());
-            existingDog.setAge(updatedDog.getAge());
-
-            // Save and return the updated dog
-            return dogRepository.save(existingDog);
-        } else {
-            // If the dog does not exist, return null or throw an exception
-            return null;
-        }
-    }
 
     public void deleteDog(Long id) {dogRepository.deleteById(id);}
 }
