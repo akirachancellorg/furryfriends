7f9de91d931380047e3885c13ca5326d3c04b8e1
Jorenzo Martin Reyes <jjreyes@student.apc.edu.ph>
2024-10-06 17:16:09 +0800

    Added Bird Pet

---
+++ src/main/java/com/entjava/furryfriends/model/Bird.java
@@ -0,0 +1,12 @@
+package com.entjava.furryfriends.model;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Bird extends Pet {
+    private String species;
+    private boolean isFlightless;
+}
